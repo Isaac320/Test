7941301f8c8950ebe1d373e2221268a08fd6e1aa
Isaac Q <943339676@qq.com>
2018-04-09 15:30:17 +0800

    add a class

---
+++ testaaa/testaaa/testttt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testaaa
+{
+    class testttt
+    {
+        public string ccc = "ddd";
+    }
+}

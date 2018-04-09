a5da434597221f21c59c150a2bf2b124b75ddd62
Isaac Q <943339676@qq.com>
2018-04-09 15:15:38 +0800

    xasxas

--- testaaa/testaaa/Form1.Designer.cs
+++ testaaa/testaaa/Form1.Designer.cs
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 371);
+            this.ClientSize = new System.Drawing.Size(589, 460);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";

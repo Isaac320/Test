4932ea2bf836fe9ce7bcff053d07953e51d2c4dc
Isaac Q <943339676@qq.com>
2018-04-09 15:11:50 +0800

    ttt

--- testaaa/testaaa/Form1.cs
+++ testaaa/testaaa/Form1.cs
@@ -16,5 +16,10 @@ namespace testaaa
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "qqq";
+        }
     }
 }
